20f5a8306de0bd8bfb7e1d235b9e8c3cb89d53f2
Edmunt <roed@onet.eu>
2015-10-16 11:33:18 +0200

    huawei_3g_lycamobile OpenWRT with support of Huawei 3g modem and Lycamobile (Poland)

---
+++ build_huawei_3g_lycamobile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+packages=( \
+    -luci \
+    -ppp -ppp-mod-pppoe \
+    -odhcpd -odhcp6c \
+    -ip6tables -kmod-ip6tables \
+    iptables-mod-ipopt kmod-ipt-conntrack \
+    ipset -dnsmasq dnsmasq-full \
+    -wpad-mini hostapd \
+    usb-modeswitch comgt-ncm \
+    kmod-usb-net-huawei-cdc-ncm \
+    block-mount kmod-usb-storage-extras \
+    kmod-fs-ext4 \
+    kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
+    kmod-fs-exfat \
+    kmod-usb-audio kmod-sound-core \
+    kmod-usb-net-dm9601-ether kmod-usb-net-asix \
+    http://downloads.openwrt.org/barrier_breaker/14.07/ramips/rt305x/packages/oldpackages/io_1_ramips_24kec.ipk )
+
+make image PROFILE=A5-V11 PACKAGES="${packages[*]}" FILES=files/huawei_3g_lycamobile/
