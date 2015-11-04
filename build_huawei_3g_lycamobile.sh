db5ae9f1f46507068a0d072038b18b6d0f9b27d2
Edmunt Pienkowsky <roed@onet.eu>
2015-11-04 15:44:41 +0100

    tc - Traffic Control

--- build_huawei_3g_lycamobile.sh
+++ build_huawei_3g_lycamobile.sh
@@ -21,23 +21,24 @@ if [ ! -d "$SCRIPT_DIR/OpenWrt-ImageBuilder-15.05-ramips-rt305x.Linux-x86_64" ];
     exit 1
 fi
 
-packages=( \
-    -luci \
-    -ppp -ppp-mod-pppoe \
-    -odhcpd -odhcp6c \
-    -ip6tables -kmod-ip6tables \
-    iptables-mod-ipopt kmod-ipt-conntrack \
-    iptables-mod-tee ipset \
-    -wpad-mini hostapd \
-    igmpproxy \
-    usb-modeswitch comgt-ncm \
-    kmod-usb-net-huawei-cdc-ncm \
-    block-mount kmod-usb-storage-extras \
-    kmod-fs-ext4 \
-    kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
-    kmod-fs-exfat \
-    kmod-usb-audio kmod-sound-core \
-    kmod-usb-net-dm9601-ether kmod-usb-net-asix \
+packages=( 
+    -luci
+    -ppp -ppp-mod-pppoe
+    -odhcpd -odhcp6c
+    -ip6tables -kmod-ip6tables
+    iptables-mod-ipopt kmod-ipt-conntrack
+    iptables-mod-tee kmod-ipt-ipset
+    tc kmod-sched
+    -wpad-mini hostapd
+    igmpproxy
+    usb-modeswitch comgt-ncm
+    kmod-usb-net-huawei-cdc-ncm
+    block-mount kmod-usb-storage-extras
+    kmod-fs-ext4
+    kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1
+    kmod-fs-exfat
+    kmod-sound-core kmod-usb-audio
+    kmod-usb-net-dm9601-ether kmod-usb-net-asix
     http://downloads.openwrt.org/barrier_breaker/14.07/ramips/rt305x/packages/oldpackages/io_1_ramips_24kec.ipk )
 
 make image -C "$SCRIPT_DIR/OpenWrt-ImageBuilder-15.05-ramips-rt305x.Linux-x86_64" PROFILE=A5-V11 PACKAGES="${packages[*]}" FILES="$SCRIPT_DIR/huawei_3g_lycamobile/"
